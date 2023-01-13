f0b2a021a9fa2075d927c61173f6d31cb5725312
Prakash Kafle <kafleprakash96@gmail.com>
2023-01-12 22:49:08 -0600

    Added Todo component for display todo items

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "../style.module.css"
+
+function Todo() {
+  return (
+    <div>
+        <div className={styles.todoitem}>
+            <h3 className={styles.todoname}>Todo Item</h3>
+            <button className={styles.deletebutton}>Done</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Todo
