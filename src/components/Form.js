5d6abe8375583ff5e5fb4fbb8169332202e420a9
Prakash Kafle <kafleprakash96@gmail.com>
2023-01-12 22:33:34 -0600

    Added form and made it functional

---
+++ src/components/Form.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from "../style.module.css"
+
+function Form({todo, setTodo, todoList, setTodoList}) {
+
+    const handleChange = (event)=>{
+        setTodo(event.target.value)
+    }
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        setTodoList([...todoList,todo]);
+    }
+
+  return (
+    <div className={styles.todoform}>
+        <form onSubmit={handleSubmit}>
+            <input value={todo} onChange={handleChange} className={styles.todoinput} placeholder='Add Todo Item' ></input>
+            <button type="submit" className={styles.todobutton}>Add</button>
+        </form>
+    </div>
+  )
+}
+
+export default Form
